87088a95d034ed498ae2ed18ea357ca6161c8f42
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2022-01-07 23:54:18 +0100

    Brak added

--- scripts/pomodoro.js
+++ scripts/pomodoro.js
@@ -2,13 +2,26 @@ const taskInput = document.querySelector("#taskname");
 const startButton = document.querySelector("#start");
 const pendingTime = document.querySelector("#pendingTime");
 const tasks = document.querySelector("#tasks");
+const takeBreakButton = document.querySelector("#take_break");
+const startBreakButton = document.querySelector("#start_break");
 
+const taskform = document.querySelector("#taskform");
+const breakform = document.querySelector("#breakform");
 
 var counterWorker;
 
 taskInput.addEventListener("input", validate);
 pendingTime.style.display= 'none';
-startButton.onclick = startTime;
+breakform.style.display= 'none';
+
+
+
+startButton.onclick = startTask;
+takeBreakButton.onclick = takeBreak;
+startBreakButton.onclick = startBreak;
+
+
+
 
 function validate(){
     if(taskInput.value === "" || checkTaskExists(taskInput.value)){
@@ -18,20 +31,21 @@ function validate(){
     }
 } 
 
-function startTime() {
+function startTask() {
     if (typeof(Worker)!==undefined) {
         pendingTime.style.display= 'block';
         startButton.setAttribute("disabled","disabled");
+        takeBreakButton.setAttribute("disabled","disabled");
         taskInput.setAttribute("disabled","disabled");
         var min = parseInt(document.getElementById('taskminutes').value)-1;
         pendingTime.innerHTML = `Pending time: ${min}:59`;
         saveAndShowTasks();
         timerWorker = new Worker("scripts/timer/timer.js");
-        var interval = window.setInterval(function(){
+        var taskInterval = window.setInterval(function(){
             timerWorker.postMessage(min);
             timerWorker.onmessage = function(e) {
                 if(!e.data) {
-                    stopCounter(interval);
+                    stopCounterTask(taskInterval);
                 } else {
                     const time = e.data;
                     min = time[0];
@@ -43,21 +57,60 @@ function startTime() {
     }
 }
 
-function stopCounter(interval) {
+
+function takeBreak() {
+    taskform.style.display= 'none';
+    breakform.style.display= 'block';
+    pendingTime.innerHTML = '';
+    pendingTime.style.display= 'block';
+}
+
+function startBreak () {
+    startBreakButton.setAttribute("disabled","disabled");
+    takeBreakButton.setAttribute("disabled","disabled");
+    var min = parseInt(document.getElementById('breakminutes').value)-1;
+    if (typeof(Worker)!==undefined) {
+        var breakInterval = window.setInterval(function(){
+            timerWorker.postMessage(min);
+            timerWorker.onmessage = function(e) {
+                if(!e.data) {
+                    stopCounterBreak(breakInterval);
+                } else {
+                    const time = e.data;
+                    min = time[0];
+                    const sec = time[1];
+                    pendingTime.innerHTML = `Pending time: ${min}:${sec > 9 ? sec : '0' + sec}`;
+                }
+            };
+        }, 100);
+    }
+}
+
+function stopCounterTask(interval) {
     clearInterval(interval);
     pendingTime.style.display= 'none';
     startButton.removeAttribute('disabled');  
+    takeBreakButton.removeAttribute('disabled');  
     taskInput.value = '';
     taskInput.removeAttribute("disabled");  
+    startBreakButton.removeAttribute("disabled");  
+}
+
+function stopCounterBreak(interval) {
+    clearInterval(interval); 
+    taskform.style.display= 'block';
+    breakform.style.display= 'none';
+    pendingTime.innerHTML = '';
+    pendingTime.style.display= 'none';
 }
 
 
 function saveAndShowTasks() {
     saveTask(taskInput.value);
     const task_list = getTasks();
-    var cadena = '<h2> Listado de tareas </h2>';
+    var cadena = '<h3> Task list </h3>';
     task_list.forEach(x => {
-        cadena += `<p> *\t${x.id} - ${x.date} </p>`
+        cadena += `<p> * ${x.id} - ${x.date} </p>`
     });
     tasks.innerHTML = cadena;
 }
