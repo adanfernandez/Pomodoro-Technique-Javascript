13a82aaa689e71929c9ae81e68208825755e1049
Adán Fernández <47212287+adanvetusta@users.noreply.github.com>
2022-01-07 20:33:49 +0100

    Checking Workers

--- scripts/pomodoro.js
+++ scripts/pomodoro.js
@@ -1,7 +1,9 @@
 const taskInput = document.querySelector("#taskname");  
 const startButton = document.querySelector("#start");
 const pendingTime = document.querySelector("#pendingTime");
-    
+
+var counterWorker;
+
 taskInput.addEventListener("input", validate);
 pendingTime.style.display= 'none';
 startButton.onclick = startTime;
@@ -15,18 +17,19 @@ function validate(){
 } 
 
 function startTime() {
-    pendingTime.style.display= 'block';
-    startButton.setAttribute("disabled","disabled");
-    taskInput.setAttribute("disabled","disabled");
-    const min = 1;
-    pendingTime.innerHTML = `Pending time: ${min-1}:59`
-    var timer = new Timer(min);
-    var interval = window.setInterval(function(){
-        const message = timer.decreaseTime();
-        pendingTime.innerHTML = message;
-        if(!timer.min && !timer.sec)  stopCounter(interval);        
-   // }, 1000);
-    }, 100);
+    if (typeof(Worker)!==undefined) {
+        pendingTime.style.display= 'block';
+        startButton.setAttribute("disabled","disabled");
+        taskInput.setAttribute("disabled","disabled");
+        const min = 1;
+        pendingTime.innerHTML = `Pending time: ${min-1}:59`
+        var interval = window.setInterval(function(){
+            counterWorker = new Worker("scripts/timer/timer.js");
+            counterWorker.postMessage(min);
+            pendingTime.onmessage = displayCounter;
+            if(!timer.min && !timer.sec)  stopCounter(interval);
+        }, 100);
+    }
 }
 
 function stopCounter(interval) {
@@ -35,4 +38,8 @@ function stopCounter(interval) {
     startButton.removeAttribute('disabled');  
     taskInput.value = '';
     taskInput.removeAttribute("disabled");  
+}
+
+function displayCounter(e) {
+    pendingTime.innerHTML = e.data;
 }
--- scripts/timer/timer.js
+++ scripts/timer/timer.js
@@ -1,23 +1,19 @@
 
-class Timer {
-    sec = 59;
-    min = 0;
+sec = 59;
+min = 0;
 
-    constructor (min) {
-        this.min = --min;
+function timer() {
+    this.sec--;
+    if(!this.sec) {
+        this.sec = 59;
+        this.min--;
     }
-
-    decreaseTime() {
-        this.sec--;
-        if(!this.sec) {
-            this.sec = 59;
-            this.min--;
-        }
-        if(this.min < 0) {
-            this.min = 0;
-            this.sec = 0;
-        }
-       return `Pending time: ${this.min}: ${this.sec > 9 ? this.sec : '0' + this.sec}`;
+    if(this.min < 0) {
+        this.min = 0;
+        this.sec = 0;
     }
+    postMessage(`Pending time: ${this.min}: ${this.sec > 9 ? this.sec : '0' + this.sec}`);
 }
 
+
+timer();
